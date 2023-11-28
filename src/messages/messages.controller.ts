57bb9279ef4ae6f1fb62ddb77af5bc6c97c36250
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-29 00:00:30 +0200

    Update

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,32 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 // Get, Post => Method Decorators
 // Body, Param => Argument Decorators
 
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor(){
+    // We will refactor code using dependency injection
+    this.messagesService = new MessagesService();
+  }
   @Get()
-  allMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    // console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    // console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
