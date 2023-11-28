cf872a300cf5c53fdd2b0359365b84ba905fe769
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-28 21:06:50 +0200

    Update

--- src/messages/dtos/create-message.dto.ts
+++ src/messages/dtos/create-message.dto.ts
@@ -2,6 +2,7 @@ import { IsString } from 'class-validator';
 
 // dto = data transfer object
 //step1. Create a class that describes the different properties that the req body should have
+
 export class CreateMessageDto{
     //step2. Add validation rules to the class
     @IsString()
